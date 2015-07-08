fbfbce903008c2884dce060ae038b296457bfe42
Moritz, David <Moritz.David@principal.com>
2015-07-08 10:05:24 -0500

    get rid of files from init-angular

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-var mainApp = angular.module('mainApp', []);
-
-mainApp.run(function runWithDependencies($rootScope) {
-	$rootScope._ = _;
-	$rootScope.moment = moment;
-	$rootScope.mc = mc;
-});
--- src/js/services/MethodFactory.js
+++
@@ -1,9 +0,0 @@
-mainApp.factory('MethodFactory', [
-	function MethodFactory() {
-		'use strict';
-
-		return {
-			// common methods
-		};
-	}
-]);
